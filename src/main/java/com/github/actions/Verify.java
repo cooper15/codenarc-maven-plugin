4388cb70bab3d551d8ed99ee3c8233f13ea49db3
cooper15 <v.centeno15@gmail.com>
2023-05-29 15:39:20 -0600

    Implemented failure strategy

--- src/main/java/com/github/actions/Verify.java
+++ src/main/java/com/github/actions/Verify.java
@@ -2,9 +2,11 @@ package com.github.actions;
 
 import com.github.dto.Parameters;
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.codenarc.CodeNarcRunner;
 import org.codenarc.analyzer.FilesystemSourceAnalyzer;
 import org.codenarc.report.HtmlReportWriter;
+import org.codenarc.results.Results;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -30,9 +32,29 @@ public class Verify {
             htmlReport.setIncludeSummaryByPackage(true);
             htmlReport.setDefaultOutputFile(parameter.getReport());
 
-            log.info("Printing code analysis report");
             runner.setReportWriters(List.of(htmlReport));
-            runner.execute();
+            Results results = runner.execute();
+
+            log.info("Checking results");
+
+            val actualPriority1 = results.getNumberOfViolationsWithPriority(1, true);
+            checkMaxViolations(actualPriority1, parameter.getMaxPriority1(), 1);
+
+            val actualPriority2 = results.getNumberOfViolationsWithPriority(2, true);
+            checkMaxViolations(actualPriority2, parameter.getMaxPriority2(), 2);
+
+            val actualPriority3 = results.getNumberOfViolationsWithPriority(3, true);
+            checkMaxViolations(actualPriority3, parameter.getMaxPriority3(), 3);
         });
+    }
+
+    private void checkMaxViolations(int actual, int expected, int priority) {
+        if (actual > expected)
+            failExecution("Priority" + priority + ": expecting less than" + expected + "violations, but found " + actual);
+    }
+
+    private void failExecution(String message) {
+        log.error(message);
+        System.exit(1);
     }
 }
